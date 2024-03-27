ee94a1166e0c1676dfed86ee913edf269ee0c7d1
Peter Marsh <pmarsh@scottlogic.com>
2024-03-27 18:44:52 +0000

    progress

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { PDFDocument, StandardFonts, rgb, degrees } from "pdf-lib";
+import PDFParser, { Output } from "pdf2json";
 import fs from "fs";
+import { line } from "./types";
 
 async function addWatermark(pdfDoc: PDFDocument) {
   const helveticaFont = await pdfDoc.embedFont(StandardFonts.Helvetica);
@@ -47,6 +49,66 @@ async function performConversion() {
   });
 }
 
-async function main() {}
+function getParsedPdfObject(filepath: string): Promise<Output> {
+  return new Promise((resolve, reject) => {
+    const pdfParser = new PDFParser();
+    pdfParser.on("pdfParser_dataError", (errData) => reject(errData));
+    pdfParser.on("pdfParser_dataReady", (pdfData) => resolve(pdfData));
+
+    pdfParser.loadPDF(filepath);
+  });
+}
+
+function logOnLine(text: string) {
+  process.stdout.write(text);
+}
+
+const SOLFEGE_SCALE = ["sol", "la", "si", "do", "re", "mi", "fa", "sol'"];
+
+async function main() {
+  const PATH = "./fileIO/in/Toc de castells (1st Gralla) with solfege.pdf";
+
+  const pdfObj = await getParsedPdfObject(PATH);
+  const firstPage = pdfObj.Pages[0];
+  const cleanedTexts = firstPage.Texts.map((text) => {
+    const concatRun = text.R.map((run) => run.T).join("");
+    return {
+      x: text.x,
+      y: text.y,
+      value: concatRun === "%09" ? " " : concatRun,
+    };
+  });
+
+  const solfegeLetters = SOLFEGE_SCALE.join("");
+  const justLetters = cleanedTexts.filter((text) =>
+    solfegeLetters.includes(text.value)
+  );
+
+  const lines: line[] = [];
+
+  // populate lines
+  justLetters.forEach((text) => {
+    const lineExists = lines.some((line) => line.y === text.y);
+    if (lineExists) {
+      const line = lines.find((line) => line.y === text.y);
+      line?.letters.push({ x: text.x, value: text.value });
+    } else {
+      lines.push({ y: text.y, letters: [{ x: text.x, value: text.value }] });
+    }
+  });
+
+  const sortedLines = lines
+    .map((line) => ({
+      y: line.y,
+      letters: line.letters.sort((a, b) => a.x - b.x),
+    }))
+    .sort((a, b) => a.y - b.y);
+
+  // print lines
+  lines.forEach((line) => {
+    const letters = line.letters.map((letter) => letter.value).join("");
+    console.log(letters);
+  });
+}
 
 main();
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+type line = {
+  y: number;
+  letters: {
+    x: number;
+    value: string;
+  }[];
+};
+
+export type { line };
