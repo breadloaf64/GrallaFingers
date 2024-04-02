6e126351e931b253d4af5caa56de4160c8293000
Peter Marsh <pmarsh@scottlogic.com>
2024-04-02 14:00:17 +0100

    more tidying

--- src/control.ts
+++ src/control.ts
@@ -2,33 +2,34 @@ import { PDFDocument } from "pdf-lib";
 import { getSolfegeForURL } from "./read";
 import fs from "fs";
 import { addWatermark } from "./modifyPDF";
+import { printSolfegeDocument } from "./print";
 
-async function AddDiagramsToAllPDFsInFolder() {
+async function ProcessFolder() {
   const IN_PATH = "./fileIO/in/";
   const OUT_PATH = "./fileIO/out/";
+
+  console.log("Processing folder: ", IN_PATH);
 
   const inputFileNames = fs.readdirSync(IN_PATH);
 
   inputFileNames.forEach(async (name) => {
+    console.log("Processing file: ", name);
     const inputFilePath = IN_PATH + name;
     const outputFilePath = OUT_PATH + name;
 
-    // get bytes
-    const existingPdfBytes = fs.readFileSync(inputFilePath);
-    const convertedPdfBytes = await modifyPdfBytes(
-      existingPdfBytes,
-      addWatermark
-    );
-    fs.writeFileSync(outputFilePath, convertedPdfBytes);
-    console.log(`Wrote modified PDF to: ${outputFilePath}`);
+    createNewPDFWithDiagrams(inputFilePath, outputFilePath);
   });
 }
 
-async function AddDiagramsToPDF(url: string) {
-  const solfege = await getSolfegeForURL(url);
+async function createNewPDFWithDiagrams(inputUrl: string, outputUrl: string) {
+  const solfege = await getSolfegeForURL(inputUrl);
 
-  const pdfBytes = fs.readFileSync(url);
+  console.log("Generated solfge:");
+  printSolfegeDocument(solfege);
+
+  const pdfBytes = fs.readFileSync(inputUrl);
   const pdfBytesModified = await modifyPdfBytes(pdfBytes, addWatermark);
+  fs.writeFileSync(outputUrl, pdfBytesModified);
 }
 
 async function modifyPdfBytes(
@@ -43,4 +44,4 @@ async function modifyPdfBytes(
   return pdfBytes;
 }
 
-export { AddDiagramsToPDF };
+export { createNewPDFWithDiagrams };
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import { getSolfegeForURL } from "./read";
-import { MIN_SOLFEGE_PER_LINE } from "./consts";
-import { printSolfegeDocument } from "./print";
+import { createNewPDFWithDiagrams } from "./Control";
 
 async function main() {
-  console.log("Parsing PDF...");
-  console.log("min solfege per line: ", MIN_SOLFEGE_PER_LINE);
-  const solfegeDocument = await getSolfegeForURL(
-    "./fileIO/in/Toc de castells (1st Gralla) with solfege.pdf"
-  );
-  printSolfegeDocument(solfegeDocument);
+  createNewPDFWithDiagrams("./fileIO/in/1.pdf", "./fileIO/out/1.pdf");
 }
 
 main();
