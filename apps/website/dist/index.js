5fde46cad73a811e2c083524f20c9107ec1f1dd2
Peter Marsh <breadloaf64@gmail.com>
2026-06-04 00:17:33 +0100

    remove dist from repo

--- apps/website/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-function helloWorld() {
-    const element = document.getElementById("hello-world");
-    if (element) {
-        element.textContent = "Hello, World!";
-    }
-}
-helloWorld();
-//# sourceMappingURL=index.js.map
